517177fdbc664fb888b773020fb1489e2dba9c97
VivekCoderz <dayanandgarg967@gmail.com>
2025-03-04 16:48:40 +0530

    Add files via upload

---
+++ portfolo/gsap.js
@@ -0,0 +1,13 @@
+// let tl = gsap.timeline();
+// tl.from(".name p",{
+//     y:-500,
+//     x:-100,
+//     duration: 1,
+
+// })
+// tl.from(".name h1",{
+//     y:-500,
+//     x:-100,
+//     duration: 1,
+
+// })
